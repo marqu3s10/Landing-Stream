e4934182c986c9e83ad5d5dcf6718971c630c265
Álvaro Marqués Ramos <marquesramosalvaro@icloud.com>
2018-11-17 10:35:56 +0100

    Structure project, some nav stuff

---
+++ source/js/core.js
@@ -0,0 +1,22 @@
+// const nav = document.querySelector(".menu");
+// const burger = document.querySelector(".burger");
+// function burgerIt(){
+//
+//   nav.classList.toggle("is-active");
+//   burger.classList.toggle("is-active");
+// }
+//
+// var selector = '.menu__link';
+//
+// $(selector).on('click', function(){
+//     $(selector).removeClass('menu__link--is-active');
+//     $(this).addClass('menu__link--is-active');
+// });
+//
+// $(selector).on('click', function(){
+//   burgerIt();
+//   $('html').animate({
+//       scrollTop: $( $(this).attr('href') ).offset().top
+//   }, 800);
+//   return false;
+// });
