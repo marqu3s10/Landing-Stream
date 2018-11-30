01ae459d33b327ed4a031bbbbe79b5ce5949fd61
Álvaro Marqués Ramos <marquesramosalvaro@icloud.com>
2018-11-30 14:10:09 +0100

    Finish animations, jank problem with twitch

--- source/js/core.js
+++ source/js/core.js
@@ -1,30 +1,13 @@
-// const nav = document.querySelector(".menu");
-// const burger = document.querySelector(".burger");
-// function burgerIt(){
-//
-//   nav.classList.toggle("is-active");
-//   burger.classList.toggle("is-active");
-// }
-//
-// var selector = '.menu__link';
-//
-// $(selector).on('click', function(){
-//     $(selector).removeClass('menu__link--is-active');
-//     $(this).addClass('menu__link--is-active');
-// });
-//
-// $(selector).on('click', function(){
-//   burgerIt();
-//   $('html').animate({
-//       scrollTop: $( $(this).attr('href') ).offset().top
-//   }, 800);
-//   return false;
-// });
 
-var header = document.getElementById("header");
+var wScroll;
+var bigName = document.querySelectorAll(".big-name > *")
+
+Array.prototype.forEach.call(bigName, function(el, i){
+  el.classList.add('is-showing')
+});
 
 window.addEventListener('scroll', function(e) {
-  var wScroll = window.scrollY;
+  wScroll = window.scrollY;
 
   // Sticky nav
   if (wScroll > 80) {
@@ -34,23 +17,55 @@ window.addEventListener('scroll', function(e) {
     header.classList.remove("slideUp");
   }
 
-  // test 1
+  // BigName
+  // if (wScroll) {
+  //   Array.prototype.forEach.call(bigName, function(el, i){
+  //     el.classList.add('is-showing')
+  //   });
+  // }
 
-  var about = document.querySelector(".about");
-  var cards = document.querySelectorAll(".card");
-  if((window.innerHeight / 1.2) > about.getBoundingClientRect().top){
 
-    Array.prototype.forEach.call(cards, function(el, i){
-      setTimeout(function(){
+  // About
+  var about = document.querySelector("section.about");
+  var specialCard = document.querySelector(".card--special");
+  var childSpecialCard = document.querySelectorAll('.card__body--special > *');
+
+  if(300 > about.getBoundingClientRect().top){
+    specialCard.classList.add('is-showing');
+    Array.prototype.forEach.call(childSpecialCard, function(el, i){
         el.classList.add('is-showing');
-      }, 250 * (i+1))
     });
+  }
 
+  var smallCard = document.querySelectorAll(".card--small");
+  Array.prototype.forEach.call(smallCard, function(el, i){
+    if(550 > el.getBoundingClientRect().top){
+      el.classList.add('is-showing');
+    }
+  });
+
+
+  // Instagram
+  var instaCard = document.querySelectorAll(".card--instagram");
+  var instaCardChildren = document.querySelector(".card--instagram > * > *");
+
+  Array.prototype.forEach.call(instaCard, function(el, i){
+    if(550 > el.getBoundingClientRect().top){
+      el.classList.add('is-showing');
+    }
+  });
+
+  // Stream
+  var stream = document.querySelector(".stream")
+  var streamContainer = document.querySelector(".stream__window");
+  var streamEmbed = '<iframe  src="https://player.twitch.tv/?channel=ahryess" width="640" height="360"  frameborder="0"   allowfullscreen="true"></iframe>'
+  if(300 > stream.getBoundingClientRect().top){
+    stream.classList.add('is-showing');
+    // streamContainer.innerHTML = streamEmbed;
+    // return;
   }
 
 });
-
-
 
 function showDescription(element){
   if(element.innerHTML == "...more"){
